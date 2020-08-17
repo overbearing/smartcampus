718cf690f83a5a2650786f13aa515d70c4dab1cd
luanweihao <luanweihao@foxmail.com>
2020-08-17 11:52:33 +0800

    Initial Commit

---
+++ Smartcampus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Smartcampus
+//
+//  Created by 蜡笔小新 on 2020/8/17.
+//  Copyright © 2020 蜡笔小新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Smartcampus/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Smartcampus
+//
+//  Created by 蜡笔小新 on 2020/8/17.
+//  Copyright © 2020 蜡笔小新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
